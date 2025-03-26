410666d11c4df62a5cb9ffb1ca1e621c4106abe2
yordi <tyordi09@gmail.com>
2025-03-26 09:43:07 +0300

    contain_duplicates.py

--- contain_duplicates.py
+++ contain_duplicates.py
@@ -1,1 +1,14 @@
-
+Class solution():
+    def contain_duplicate(self,nums:List[int]) -> bool:
+        n-len(nums)
+        for i in range (n-1):
+            for j in range(i+1,n):
+                if nums[i]==nums[j]:
+                    return True
+            return False
+sol=solution()
+number=input("Enter Your number:")
+nums=list(map(int,number.split()))
+result=sol.contain_duplicate(nums)
+print(result)
+ 
